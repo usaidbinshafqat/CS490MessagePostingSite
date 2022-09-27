baad32bc43e6dc7703bd6bc63eb1b6c0ca16bd5b
Usaid Bin Shafqat <usaid.shafqat@gmail.com>
2022-09-27 18:39:02 -0400

    Added Modal and Post UI
    (everything is placeholder)
    added modal (now controlled through redux on FAB press)
    Added new library (react-mentions) which we'll use for hashtags and mentions. This will also render the TextField (needs work later).

--- src/store/slice.ts
+++ src/store/slice.ts
@@ -3,23 +3,22 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 
 const initialState = { 
-    homePageActive: 0
+    homePageActive: 0, 
+    modalOpen: false,
 }
 
-export const name = 'counter'
+export const name = 'reducers'
 
 const slice = createSlice({
     name, 
     initialState, 
     reducers: {
         changeTab: (state, action: PayloadAction<number>) => { 
-            // if (state.homePageActive === "Home") {
-            //     state.homePageActive = "Trends"
-            // } else {
-            //     state.homePageActive = "Home"
-            // }
             state.homePageActive = action.payload
-            console.log("Testing home page state",state.homePageActive)
+        },
+        toggleModal: (state) => { 
+            state.modalOpen = !state.modalOpen
+            console.log("Testing modal open state", state.modalOpen)
         }
     }
 })
