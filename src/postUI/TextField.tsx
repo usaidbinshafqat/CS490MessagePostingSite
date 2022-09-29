ef07360a24a1079ebbd1df88117b81dbde002bc5
Usaid Bin Shafqat <usaid.shafqat@gmail.com>
2022-09-29 18:53:16 -0400

    Added basic library and UI for new post

--- src/postUI/TextField.tsx
+++ src/postUI/TextField.tsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { TextField } from "@mui/material";
+import { useState, useCallback } from "react";
 import { MentionsInput, Mention } from "react-mentions";
 
-export const TextField = () => {
+export const InputTextField = () => {
   const data = [
     {
       id: "1",
       display: "John Doe",
     },
+    {
+      id: "2",
+      display: "Jane Doe",
+    },
+    {
+      id: "3",
+      display: "Jack Doe",
+    },
   ];
 
   const [value, setValue] = useState("");
+
   return (
-    <MentionsInput
-      singleLine
-      value={value}
-      placeholder={"Mention people using '@'"}
-      a11ySuggestionsListLabel={"Suggested mentions"}
-    >
-      <Mention data={data} trigger="@" />
-    </MentionsInput>
+    // <MentionsInput
+    //   value={value}
+    //   placeholder={"Mention people using '@'"}
+    //   a11ySuggestionsListLabel={"Suggested mentions"}
+    //   defaultValue="Hello @johndoe"
+    // >
+    //   <Mention data={data} trigger="@" />
+    // </MentionsInput>
+
+    <TextField />
   );
 };
