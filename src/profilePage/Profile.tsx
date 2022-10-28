c32c20260a163f6af6ace5e23d20e9a1e1419f54
kydogg <87150316+kydogg@users.noreply.github.com>
2022-10-28 16:03:29 -0400

    Profile styling
    more styling. next up: pushing follow button next

--- src/profilePage/Profile.tsx
+++ src/profilePage/Profile.tsx
@@ -6,26 +6,34 @@ import { CardUI } from "../cards/CardUI";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import './Profile.css';
 // ? does it need to take in any parameters?
 export const Profile = () => {
 	return (
 		<div>
 			{/* user profile picture */}
-			<Grid  spacing={-30} container>
+			<Grid spacing={-30} container>
 				<Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
 				<Box sx={{ "& > :not(style)": { m: 1 } }}>
-					<Fab
+					<Fab className="profile__info"
 						variant="extended"
 						size="medium"
 						color="primary"
 						aria-label="add"
 					>
 						Follow
-					</Fab>
+          </Fab>
+          	<Typography className="profile__info" variant="h4" gutterBottom>
+				Kyle Baker
+			</Typography>
+			<Typography className="profile__info" variant="subtitle1" gutterBottom>
+				@kydoggbaker
+			</Typography>
 				</Box>
 			</Grid>
 
-			<h4>Username</h4>
+		
 
 			{/* posts */}
 			<CardUI />
